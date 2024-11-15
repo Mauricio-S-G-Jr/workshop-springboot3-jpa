2e17dd78f4ab580ed5fdad483df324c992f31be8
Mauricio Gonçalves <mauriciosgjr@proton.me>
2024-11-15 16:12:07 -0300

    Category and Fix Serialable long atribute

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -17,7 +17,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable{
-    private static final Long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
